dcd924a6c8fa27066fd0aeb82187db5be33e6139
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-17 15:45:30 +0200

    almost done

--- dom/script.js
+++ dom/script.js
@@ -5,7 +5,7 @@ console.log("probando")
 
 console.log(document)
 
-// querySelector => metodo que nos permite hacer busquedas en el documento
+// querySelector => metodo que nos permite hacer busquedas en el documento. Consigue el PRIMER elemento que concuerde con la busqueda
 
 let h1Node = document.querySelector("h1") // nos devuelve la referencia de ese elemento
 
@@ -49,3 +49,60 @@ buttonNode.addEventListener("click", () => {
   console.log("presionando el button")
   h3Node.innerText += "Patata"
 })
+
+
+
+// Otro tipo de selecciones
+
+let titleNode = document.querySelector("#title")
+titleNode.innerText = "Cambiando Titulo"
+
+let paragNode = document.querySelector(".some-text")
+paragNode.innerText = "Cambiando parrafo"
+
+// querySelectorAll => busquedas de multiples elementos que concuerden con la busqueda
+
+let allParagNode = document.querySelectorAll(".some-text")
+console.log(allParagNode)
+
+// el listas de nodos podemos:
+// 1. usar notacion de corchete
+// 2. podemos hacer bucles tradicionales
+// 3. forEach
+
+allParagNode[2].innerText = "Modificando solo el 3er parrafo"
+
+for (let i = 0; i < allParagNode.length; i++) {
+  allParagNode[i].innerText = "modificando con bucle for"
+}
+
+allParagNode.forEach((eachNode) => {
+  eachNode.innerText = "modificando con el forEach"
+})
+
+// getElementById("algun-id")
+// getElementsByClassName("alguna-clase")
+// getElementsByTagName("etiqueta")
+
+//  Crear y eliminar elementos del DOM
+
+let containerNode = document.querySelector("#empty-container")
+
+// createElement => crea un nuevo elemento de DOM para ser agregado a la pagina
+let newNode = document.createElement("h2") // ref 1234
+console.log(newNode)
+newNode.innerText = "Nuevo Titulo";
+
+// append => metodo de cualquier elemento de DOM para añadir otro elemento adentro
+
+setTimeout(() => {
+  // dondeAñadir.append( queAñadir )
+  containerNode.append( newNode ) // ref 1234
+}, 1000)
+
+
+// .remove() => metodo de cualquier elemento de DOM para removerlo de la visualizacion (DOM)
+
+setTimeout(() => {
+  newNode.remove() // ref 1234
+}, 3000)
